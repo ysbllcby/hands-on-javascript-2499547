c205f82228aa2e6a3ff0d4e061588f10abb8d2e8
ysbllcby <121826703+ysbllcby@users.noreply.github.com>
2023-02-07 20:16:17 +0000

    Functions to grab values from smaller functions

--- 01_02b/script.js
+++ 01_02b/script.js
@@ -8,18 +8,18 @@ import data from "./data.js";
 
 const mainContent = document.querySelector(".main-content");
 
-const Card = (data) => {
-  const imgData = data[0];
-  const date = new Date(imgData.created_at);
+const buildImage = (imgData) => {
+  let srcset = `${imgData.urls.full} ${imgData.width}w`;
+  if (imgData.urls.regular) {
+    srcset = srcset + `, ${imgData.urls.regular} 1080w`;
+  }
+  if (imgData.urls.small) {
+    srcset = srcset + `, ${imgData.urls.small} 400w`;
+  }
 
-  const markup = `
-    <figure class="image">
-      <img
-        srcset="
-          ${imgData.urls.full} ${imgData.width}w,
-          ${imgData.urls.regular} 1080w,
-          ${imgData.urls.small} 400w
-        "
+  const img = `
+  <img
+        srcset="${srcset}"
         sizes="(max-width: 450px) 400px, (max-width: 800) 1080px"
         src="${imgData.urls.regular}"
         width="${imgData.width}"
@@ -27,6 +27,26 @@ const Card = (data) => {
         alt="${imgData.description}"
         loading="lazy"
       />
+  `;
+  return img;
+};
+
+const getDate = (imgData) => {
+  const date = new Date(imgData.created_at);
+  const niceDate = date.toLocaleString("default", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  return niceDate;
+};
+
+const Card = (data) => {
+  const imgData = data[0];
+
+  const markup = `
+    <figure class="image">
+      ${buildImage(imgData)}
       <figcaption class="image__caption">
         <h3 class="image__title">${imgData.description}</h3>
         <div class="image__meta">
@@ -35,14 +55,9 @@ const Card = (data) => {
             <span class="image__photog">${imgData.user.name}</span>.
           </p>
           <p>
-            Uploaded on
-            <time class="image__date" datetime="${imgData.created_at}">
-            ${date.toLocaleString("default", {
-              year: "numeric",
-              month: "long",
-              day: "numeric",
-            })}
-            </time>.
+            Uploaded on <time class="image__date" datetime="${
+              imgData.created_at
+            }">${getDate(imgData)}</time>.
           </p>
           <p>
             <a href="${imgData.links.self}" class="image__link">
