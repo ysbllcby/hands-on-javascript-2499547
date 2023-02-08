fd3e62865f7a44cae448bd557c259c8f92c7106e
ysbllcby <121826703+ysbllcby@users.noreply.github.com>
2023-02-08 22:23:10 +0000

    Use of Weather API!

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 export default {
   // Get API key from https://home.openweathermap.org/users/sign_up
-  appid: [OpenWeatherAPIkey],
+  appid: "1df9522859971edb3361be0cb099aa19",
   units: "metric",
-  location: "carpinteria,ca,us",
+  location: "calgary,ca",
 };
